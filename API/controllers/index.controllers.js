09bbaad0e6ecca190e4e33fd9ae1de21a14885fb
Isaac Yael Zaragoza Oldendorff <isaac.oldendorff@gmail.com>
2025-04-01 23:54:48 -0600

    Add files via upload

---
+++ API/controllers/index.controllers.js
@@ -0,0 +1,3 @@
+export const raiz = (req, res) => res.send("Hello World from my API!");
+export const marco = (req, res) => res.send("polo");
+export const ping = (req, res) => res.send("pong");
---
+++ API/controllers/login.controllers.js
@@ -0,0 +1,27 @@
+import { connectDB } from "../utils/sql.js";
+import { hashPassword } from "../utils/hash.js";
+
+export const login = async (req, res) => {
+  const sql = connectDB();
+  const query = {
+    text: "SELECT * FROM users WHERE username = $1",
+    values: [req.body.username],
+  };
+  const data = await sql.query(query);
+  // console.log(data.rows);
+  // console.log(data.rows.length);
+  if (data.rows.length === 0) {
+    res.json({ isLogin: false, user: {} });
+    return;
+  }
+
+  const salt = data.rows[0].password.substring(0, process.env.SALT);
+  const hash = hashPassword(req.body.password, salt);
+  const saltedHash = salt + hash;
+  if (saltedHash === data.rows[0].password) {
+    res.json({ isLogin: true, user: data.rows[0] });
+    return;
+  } else {
+    res.json({ isLogin: false, user: {} });
+  }
+};
